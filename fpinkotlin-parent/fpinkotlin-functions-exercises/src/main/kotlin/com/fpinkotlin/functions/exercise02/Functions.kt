2df9beb688bd30d1b1b77a8e91ec564f134cad82
Jordan Bergin <jordan.j.bergin@protonmail.com>
2021-02-11 22:58:15 +0000

    Answered Functions Exercises 1 and 2

--- fpinkotlin-parent/fpinkotlin-functions-exercises/src/main/kotlin/com/fpinkotlin/functions/exercise02/Functions.kt
+++ fpinkotlin-parent/fpinkotlin-functions-exercises/src/main/kotlin/com/fpinkotlin/functions/exercise02/Functions.kt
@@ -6,3 +6,6 @@ fun square(n: Int) = n * n
 fun triple(n: Int) = n * 3
 
 // Define a generic compose. The exercise dos not consist in writing the implementation, but the signature.
+fun <Input, Middle, Output> compose(f: (Middle) -> Output, g: (Input) -> Middle): (Input) -> Output = {
+    f(g(it))
+}
